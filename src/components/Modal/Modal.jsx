3bf387cf14177d8a547575d3505650a7a6f3d2db
DaldenU <daldenuteryag@gmail.com>
2025-03-12 14:34:15 +0500

    created 'Modal' component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Modal.module.css";
+
+const Modal = ({children, onClose}) => {
+    return (
+        <div className={styles.modalOverlay} onClick={onClose}>
+            <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+                <button className={styles.closeButton} onClick={onClose}>
+                    &times;
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
