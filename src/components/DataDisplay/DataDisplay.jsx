ac95c5f78a21e48d70330d8e558b4accdff3ea42
DaldenU <daldenuteryag@gmail.com>
2025-03-12 15:20:13 +0500

    Created 'Data Display' component

---
+++ src/components/DataDisplay/DataDisplay.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {fetchLast10Wells} from "../../axios/wellService";
+import styles from "./DataDisplay.module.css";
+import PumpIcon from "../../assets/pump_icon.png";
+import Modal from "../../components/Modal/Modal";
+import ResponsiveTable from "../../components/ResponsiveTable/ResponsiveTable";
+
+export default function DataDisplay({ label, value, clickable = false }) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [data, setData] = useState([]);
+
+    const handleClick = async () => {
+        if (!clickable) return;
+        else {
+            try {
+                const response = await fetchLast10Wells();
+                console.log(response.data);
+                setData(response.data);
+                setIsModalOpen(true);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+    };
+
+    return (
+        <>
+            <div className={styles.container} onClick={handleClick} style={{cursor: clickable ? "pointer" : "default"}}>
+                <div className={styles.iconContainer}>
+                    <img src={PumpIcon} alt="Icon" className={styles.icon} />
+                </div>
+                <div className={styles.label}>{label}</div>
+                <div className={styles.value}>{value}</div>
+            </div>
+
+            {isModalOpen && (
+                <Modal onClose={() => setIsModalOpen(false)}>
+                    <ResponsiveTable data={data} />
+                </Modal>
+            )}
+        </>
+    );
+}
