d39de659cdb9a5fcf1252ff499adadd669a1f5c1
DaldenU <daldenuteryag@gmail.com>
2025-03-13 13:52:56 +0500

    created 'Label Box' component

---
+++ src/components/LabelBox/LabelBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./LabelBox.module.css";
+
+export default function LabelBox({label,width,height,fontSize}) {
+    return (
+        <div className={styles.labelContainer} style={{width:width, height:height,fontSize:fontSize}}
+        >
+            {label}
+        </div>
+    );
+}
