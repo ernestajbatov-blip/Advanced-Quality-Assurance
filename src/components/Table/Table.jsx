bbe7ee2c71f018d5794efd6db1a0bd7ae1a16a78
DaldenU <daldenuteryag@gmail.com>
2025-03-14 11:10:07 +0500

    created 'Table' component

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Table.module.css";
+
+export default function Table({data}) {
+    return (
+        <div className={styles.tableContainer}>
+            <div className={styles.tableBody}>
+                {data.map((row,index) => (
+                    <div className={styles.tableRow} key={index}>
+                        <div className={styles.tableCell}>{row.value}</div>
+                        <div className={styles.tableCell}>{row.value}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
