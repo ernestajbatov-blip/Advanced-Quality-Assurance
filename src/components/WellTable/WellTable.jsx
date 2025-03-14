62ff87acd391b43d77b844fc7163dfad80fa3ad2
DaldenU <daldenuteryag@gmail.com>
2025-03-14 10:59:36 +0500

    created 'Well Table' component

---
+++ src/components/WellTable/WellTable.jsx
@@ -0,0 +1,54 @@
+import React, {useContext} from "react";
+import styles from "./WellTable.module.css";
+import{WellsABCContext} from "../../states/WellsABCContext";
+
+export default function WellTable({wells, setSelectedWell}) {
+    const context = location.pathname === "/abc" ? useContext(WellsABCContext) : null;
+
+    const handleWellClick = (wellName) => {
+        const {wells} = context;
+
+        const filteredWells = wells.filter((well) => well.well === wellName);
+        setSelectedWell(filteredWells);
+    };
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString("ru-RU", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+        });
+    };
+
+    return (
+        <div className={styles.tableContainer}>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>Скважина</th>
+                        <th>Замер Qж</th>
+                        <th>Пред Qж</th>
+                        <th>обводн.</th>
+                        <th>Пред. обв.</th>
+                        <th>Тех. реж. обв.</th>
+                        <th>Дата</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {wells.map((well, index) => (
+                        <tr key={index} onClick={() => handleWellClick(well.well)}>
+                            <td>{well.well}</td>
+                            <td>{well.tm_fluid.toFixed(2)}</td>
+                            <td>{well.tm_fluid_prev.toFixed(2)}</td>
+                            <td>{well.tm_water.toFixed(2)}</td>
+                            <td>{well.tm_water_prev.toFixed(2)}</td>
+                            <td>{well.tr_water.toFixed(2)}</td>
+                            <td>{formatDate(well.date)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
