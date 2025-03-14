fc343cab1da2a957c7c774c4a19fea148dd92254
DaldenU <daldenuteryag@gmail.com>
2025-03-14 11:36:28 +0500

    created 'Progress Bar' component

---
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./ProgressBar.module.css";
+
+export default function ProgressBar({value, maxValue, color, wodth, height}) {
+    const percentage = (value / maxValue) * 100;
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.barContainer} style={{width: `${width}px`, height: `${height}px`}}>
+                <div className={styles.progress} style={{height: `${percentage}%`, backgroundColor: color}}>
+
+                </div>
+            </div>
+            <div className={styles.label} style={{bottom: `calc(${percentage}% - 8px)`, left: `${width + 5}px`}}>
+                {value} см
+            </div>
+        </div>
+    );
+}
