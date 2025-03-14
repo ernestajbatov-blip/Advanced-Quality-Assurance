bcbc3583aad6d43f4d181dc3e0298e982c9057a7
DaldenU <daldenuteryag@gmail.com>
2025-03-14 14:08:58 +0500

    created 'Grid' component

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Grid.module.css";
+import WellCard from "../WellCard/WellCard";
+
+export default function Grid({wells, fieldMappings, calculateMiddleValue}) {
+    return (
+        <div className={styles.gridContainer}>
+            {wells.map((well, index) => {
+                const leftBottomValue = well[fieldMappings.leftBottom];
+                const rightTopValue = well[fieldMappings.rightTop];
+                const middleValue = calculateMiddleValue ? calculateMiddleValue(rightTopValue, leftBottomValue) : well[fieldMappings.middle];
+
+                return (
+                    <WellCard
+                        key={index}
+                        leftTop={well[fieldMappings.leftTop]}
+                        rightTop={rightTopValue}
+                        middle={middleValue}
+                        leftBottom={leftBottomValue}
+                        rightBottom={well[fieldMappings.rightBottom]}
+                    />
+                );
+            })}
+        </div>
+    );
+}
